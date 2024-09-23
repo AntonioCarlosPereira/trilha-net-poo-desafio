fc08283eb27f47626a392f80930025767f6c5c8b
AntonioCarlosPereira <4nt.556@gmail.com>
2024-09-23 15:52:15 -0300

    feito2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando instâncias das classes Nokia e Iphone
+        Smartphone nokia = new Nokia("123456789", "Nokia 3310", 16, "111222333444555");
+        Smartphone iphone = new Iphone("987654321", "iPhone 13", 128, "555444333222111");
+
+        // Testando os métodos das classes Nokia e Iphone
+        Console.WriteLine("Testando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
